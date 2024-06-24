63edf8df4258dc46df6f524a53fd0b5a41f17f2d
DaydreamingDragon <jsperera2007@gmail.com>
2024-06-24 17:00:10 -0400

    created helpers.py and added list of cards

---
+++ helpers.py
@@ -0,0 +1,14 @@
+cards = ["ace", "ace", "ace", "ace",
+         "king", "king", "king", "king",
+         "queen", "queen", "queen", "queen",
+         "jack", "jack", "jack", "jack",
+         10, 10, 10, 10,
+         9, 9, 9, 9,
+         8, 8, 8, 8,
+         7, 7, 7, 7,
+         6, 6, 6, 6,
+         5, 5, 5, 5,
+         4, 4, 4, 4,
+         3, 3, 3, 3,
+         2, 2, 2, 2
+         ]
